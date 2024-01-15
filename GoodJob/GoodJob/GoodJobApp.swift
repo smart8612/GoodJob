cc06645ffbf69cb82206db8c294db093a726e5ef
JeongTaek Han <smart8612@gmail.com>
2024-01-15 15:35:57 +0900

    file: CoreData 프로젝트 환경 구성 #2

---
+++ GoodJob/GoodJob/GoodJobApp.swift
@@ -0,0 +1,20 @@
+//
+//  GoodJobApp.swift
+//  GoodJob
+//
+//  Created by JeongTaek Han on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct GoodJobApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
