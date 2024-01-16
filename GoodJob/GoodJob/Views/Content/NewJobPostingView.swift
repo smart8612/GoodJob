544ad02165582245c1d09af4ad1c7ca1d3ddb529
JeongTaek Han <smart8612@gmail.com>
2024-01-16 16:25:01 +0900

    feat: JobPosting 텍스트 필드 추가

--- GoodJob/GoodJob/Views/Content/NewJobPostingView.swift
+++ GoodJob/GoodJob/Views/Content/NewJobPostingView.swift
@@ -15,10 +15,35 @@ struct NewJobPostingView: View {
     
     private let title = "Are you sure want to discard this new job posting?"
     
+    @State private var isEditing = false
+    
+    @State private var companyName: String = ""
+    @State private var jobPosition: String = ""
+    @State private var workplaceLocation: String = ""
+    @State private var recruitNumbers: String = ""
+    @State private var link: String = ""
+    
+    @State private var startDate: Date = Date()
+    @State private var endDate: Date = Date()
+    
     var body: some View {
         NavigationStack {
-            List {
-                Text("Hello World")
+            Form {
+                Section {
+                    TextField("Company Name", text: $companyName)
+                }
+                
+                Section {
+                    TextField("Job Position", text: $jobPosition)
+                    TextField("Workplace Location", text: $workplaceLocation)
+                    TextField("Recruitment Numbers", text: $recruitNumbers)
+                    TextField("Job Posting Link", text: $link)
+                }
+                
+                Section {
+                    DatePicker(selection: $startDate, label: { Text("Starts") })
+                    DatePicker(selection: $endDate, label: { Text("Ends") })
+                }
             }
             .navigationTitle("Hello World")
             .navigationBarTitleDisplayMode(.inline)
