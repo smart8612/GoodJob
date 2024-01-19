f5973a4cc1108fa12cee27040a220e7002ce6ff8
JeongTaek Han <smart8612@gmail.com>
2024-01-19 15:45:14 +0900

    feat: CDTestRecord 헬퍼 익스텐션 구현 #6

---
+++ GoodJob/GoodJob/Data/Extensions/CDTestRecord+Extensions.swift
@@ -0,0 +1,46 @@
+//
+//  CDTestRecord+Extensions.swift
+//  GoodJob
+//
+//  Created by JeongTaek Han on 1/19/24.
+//
+
+import Foundation
+import CoreData
+
+
+extension CDTestRecord {
+    
+    public var id: UUID {
+        get { id_ ?? .init() }
+        set { id_ = newValue }
+    }
+    
+    var memo: String {
+        get { memo_ ?? .init() }
+        set { memo_ = newValue }
+    }
+    
+    var jobApplication: CDJobApplication {
+        get { jobApplication_! }
+        set { jobApplication_ = newValue }
+    }
+    
+    var test: CDTest {
+        get { test_! }
+        set { test_ = newValue }
+    }
+    
+    convenience init(memo: String, jobApplication: CDJobApplication, test: CDTest,
+                     context: NSManagedObjectContext) {
+        self.init(context: context)
+        self.memo = memo
+        self.jobApplication = jobApplication
+        self.test = test
+    }
+    
+    public override func awakeFromInsert() {
+        self.id = .init()
+    }
+    
+}
