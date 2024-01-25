5d6f3af435ccd5fef59ebf1808c132b5aca81e09
JeongTaek Han <smart8612@gmail.com>
2024-01-25 14:43:45 +0900

    feat: JobApplication Core Data Observer 구현 #8

--- GoodJob/GoodJob/Business/Data Observers/GJJobApplicationDataObserver.swift
+++ GoodJob/GoodJob/Business/Data Observers/GJJobApplicationDataObserver.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import CoreData
+
+
+final class GJJobApplicationDataObserver: NSObject, GJDataObserver {
+    
+    weak var delegate: GJDataObserverDelegate?
+    
+    private let persistenceController: PersistenceController
+    
+    private lazy var fetchedResultController: NSFetchedResultsController<CDJobApplication>? = {
+        let fetchRequest = CDJobApplication.fetchRequest()
+        fetchRequest.predicate = NSPredicate(value: true)
+        fetchRequest.sortDescriptors = [
+            NSSortDescriptor(
+                keyPath: \CDJobApplication.createdAt_, ascending: false)
+        ]
+        let controller = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: persistenceController.managedObjectContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
+        try? controller.performFetch()
+        return controller
+    }()
+    
+    init(delegate: GJDataObserverDelegate? = nil, persistenceController: PersistenceController = .shared) {
+        self.persistenceController = persistenceController
+        super.init()
+        self.delegate = delegate
+        self.fetchedResultController?.delegate = self
+    }
+    
+}
+
+extension GJJobApplicationDataObserver: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        delegate?.dataWillChange()
+    }
+    
+}
