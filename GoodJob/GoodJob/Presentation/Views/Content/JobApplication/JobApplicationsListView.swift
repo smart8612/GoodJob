d19cc0443918f89d1f5aa36ef2d78f08167c3b02
JeongTaek Han <smart8612@gmail.com>
2024-01-28 16:47:12 +0900

    feat: 시각적으로 채용 공고 유효성 확인 가능 #10

--- GoodJob/GoodJob/Presentation/Views/Content/JobApplication/JobApplicationsListView.swift
+++ GoodJob/GoodJob/Presentation/Views/Content/JobApplication/JobApplicationsListView.swift
@@ -51,13 +51,38 @@ fileprivate struct JobApplicationCellView: View {
     
     let jobApplication: GJJobApplication
     
+    private let jobPostingController: GJJobPostingController = {
+        GJJobPostingController(
+            jobPostingRepository: GJJobPostingRepository(),
+            testRepository: GJTestRepository()
+        )
+    }()
+    
+    @State private var jobPosting: GJJobPosting? = nil
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(jobApplication.title)
-                .font(.headline)
-            Text(jobApplication.id.uuidString)
-                .font(.caption)
+        Group {
+            if let jobPosting = jobPosting {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(jobApplication.title)
+                        .font(.headline)
+                    VStack(alignment: .leading, spacing: 2) {
+                        Label(jobPosting.companyName, systemImage: "building.2")
+                        Label(jobPosting.jobPositionName, systemImage: "figure.walk")
+                        Label(jobApplication.createdAt.formatted(), systemImage: "gauge.with.needle")
+                    }
+                    .font(.caption2)
+                }
+            } else {
+                Text("Loading...")
+            }
+        }
+        .onAppear {
+            do {
+                self.jobPosting =  try jobPostingController.fetchJobPosting(with: jobApplication.jobPostingId)
+            } catch {
+                print(error.localizedDescription)
+            }
         }
     }
 }
-
--- GoodJob/GoodJob/Presentation/Views/Content/JobPosting/JobPostingsListView.swift
+++ GoodJob/GoodJob/Presentation/Views/Content/JobPosting/JobPostingsListView.swift
@@ -51,16 +51,49 @@ fileprivate struct JobPostingCellView: View {
     let jobPosting: GJJobPosting
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Text(jobPosting.jobPositionName)
-                .font(.headline)
-            VStack(alignment: .leading) {
-                Label(jobPosting.companyName, systemImage: "building.2")
-                Label(jobPosting.workplaceLocation, systemImage: "globe")
-                Label(jobPosting.endDate.formatted(), systemImage: "calendar")
+        HStack(alignment: .center, spacing: 16) {
+            ConvenienceDateDisplay(date: jobPosting.endDate)
+                .frame(width: 80, height: 80)
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text(jobPosting.jobPositionName)
+                    .font(.headline)
+                VStack(alignment: .leading, spacing: 2) {
+                    Label(jobPosting.companyName, systemImage: "building.2")
+                    Label(jobPosting.workplaceLocation, systemImage: "globe")
+                    Label(jobPosting.endDate.formatted(), systemImage: "calendar")
+                }
+                .font(.caption2)
             }
-            .font(.caption2)
+            
+            Spacer()
         }
+    }
+}
+
+
+#Preview {
+    ConvenienceDateDisplay(date: .init(timeIntervalSinceNow: -20))
+}
+
+struct ConvenienceDateDisplay: View {
+    
+    let date: Date
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .foregroundStyle(shapeColor)
+            .overlay {
+                Text(message)
+            }
+    }
+    
+    var message: String {
+        (date < .now) ? "End":"Ing"
+    }
+    
+    var shapeColor: Color {
+        (date < .now) ? .red:.green
     }
 }
 
