bb047138828e58090a960039072927740b3f6422
JeongTaek Han <smart8612@gmail.com>
2024-02-01 17:10:55 +0900

    feat: 커스텀 팔레트 컬러 확장 구현 #14

---
+++ GoodJob/GoodJob/Resources/Extensions+Color.swift
@@ -0,0 +1,19 @@
+//
+//  Extensions+Color.swift
+//  GoodJob
+//
+//  Created by JeongTaek Han on 2/1/24.
+//
+
+import SwiftUI
+
+
+extension Color {
+    static let primaryColor = Color("AppPrimaryColor")
+    static let secondaryColor = Color("AppSecondaryColor")
+    static let backgroundColor = Color("BackgroundColor")
+    static let textColor = Color("TextColor")
+    static let errorColor = Color("ErrorColor")
+    static let successColor = Color("SuccessColor")
+    static let neutralColor = Color("NeutralColor")
+}
